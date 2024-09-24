d2b9e09b13226ed8a6ca996167f1486232d8b29a
KarolusPaulus <karosaarinenn@gmail.com>
2024-09-24 19:11:38 +0300

    Update Hello World message

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World! :DD');
 });
 
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`);
 });
-//:))
